3e7fc80bbc6931a36a019efd5c399dc2edfe354e
Denis <chalard.d@hotmail.fr>
2022-03-11 03:38:19 +0100

    p5

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,5 +1,7 @@
 var products = JSON.parse(localStorage.getItem('cart') || "[]");
 
+var totalPrice = 0;
+
 //displaying cart
 function displayCart(products){
   //setting the div that will contain our display
@@ -8,11 +10,11 @@ function displayCart(products){
     //foreach product in our data we get from API
 
     products.forEach(element => {
-      var product = document.createElement("article");
-        product.className = "cart__item";
-        product.setAttribute('data-id', element.id);
-        product.setAttribute('data-color', element.color)
-        product.innerHTML = ' \
+      var productArticle = document.createElement("article");
+      productArticle.className = "cart__item";
+      productArticle.setAttribute('data-id', element.id);
+      productArticle.setAttribute('data-color', element.color)
+      productArticle.innerHTML = ' \
             <div class="cart__item__img"> \
               <img src="'+element.imageUrl+'" alt="'+element.altTxt+'"> \
             </div> \
@@ -25,31 +27,36 @@ function displayCart(products){
               <div class="cart__item__content__settings"> \
                 <div class="cart__item__content__settings__quantity"> \
                   <p>Qté : '+element.number+'</p> \
-                  <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="'+element.number+'"onClick="updatingFromCart(\''+element.id+'\',\''+element.color+'\')"> \
+                  <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="'+element.number+'" onClick="updatingFromCart(\''+element.id+'\',\''+element.color+'\',\''+parseInt(element.number) * parseInt(element.price)+'\')"> \
                 </div> \
                 <div class="cart__item__content__settings__delete"> \
-                  <p class="deleteItem" onClick=\"removeFromCart(\''+element.id+'\',\''+element.color+'\')">Supprimer</p> \
+                  <p class="deleteItem" onClick=\"removeFromCart(\''+element.id+'\',\''+element.color+'\',\''+parseInt(element.number) * parseInt(element.price)+'\')">Supprimer</p> \
                 </div> \
               </div> \
             </div>';
-          productContainer.appendChild(product);
+            totalPrice += parseInt(element.number) * parseInt(element.price);
+            productContainer.appendChild(productArticle);
   });
+  
+document.getElementById("totalPrice").innerHTML = totalPrice;
 }
 
 //deleting product
-function removeFromCart(id,color){
+function removeFromCart(id,color, total){
   //if the current product and color does no exist
   products = products.filter((item) => (item.uuid !== id.toString()+color.toString()));
+  document.querySelector("[data-id='"+id+"'][data-color='"+color+"']").outerHTML = "";
+
+  //updating price
+  totalPrice -= total;
+  document.getElementById("totalPrice").innerHTML = totalPrice;
 
   //set the products list into the localStorage
   localStorage.setItem('cart', JSON.stringify(products));
-
-  //displaying product
-  displayCart(products);
 }
 
 //updating product
-function updatingFromCart(id,color){
+function updatingFromCart(id,color,total){
   var number = parseInt(document.querySelector("[data-id='"+id+"'][data-color='"+color+"'] .itemQuantity").value);
   var uuid = id.toString()+color.toString();
 
@@ -58,14 +65,17 @@ function updatingFromCart(id,color){
     return product.uuid === uuid;
   });
 
+  //updating price
+  totalPrice -= products[result].number*products[result].price;
+  totalPrice += number*products[result].price;
+  document.getElementById("totalPrice").innerHTML = totalPrice;
+
   //if the current product and color does no exist
   products[result].number = number;
+  document.querySelector("[data-id='"+id+"'][data-color='"+color+"'] .cart__item__content__settings__quantity p").innerHTML = 'Qté : '+number;
 
   //set the products list into the localStorage
   localStorage.setItem('cart', JSON.stringify(products));
-
-  //displaying product
-  displayCart(products);
 }
 
 //displaying product
